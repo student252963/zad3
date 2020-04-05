91d5c7f27e447b563697ee6ab72981650a5cc214
Kacper Raczyński <252963@student.pwr.edu.pl>
2020-04-05 22:40:14 +0200

    Poczatkowe pliki

---
+++ inc/Macierz.hh
@@ -0,0 +1,42 @@
+#ifndef MACIERZ_HH
+#define MACIERZ_HH
+
+#include "rozmiar.h"
+#include <iostream>
+
+
+/*
+ *  Tutaj trzeba opisac klase. Jakie pojecie modeluje ta klasa
+ *  i jakie ma glowne cechy.
+ */
+class Macierz {
+  /*
+   *  Tutaj trzeba wstawic definicje odpowiednich pol i metod prywatnych
+   */
+  public:
+  /*
+   *  Tutaj trzeba wstawic definicje odpowiednich metod publicznych
+   */    
+};
+
+
+/*
+ * To przeciazenie trzeba opisac. Co ono robi. Jaki format
+ * danych akceptuje. Jakie jest znaczenie parametrow itd.
+ * Szczegoly dotyczace zalecen realizacji opisow mozna
+ * znalezc w pliku:
+ *    ~bk/edu/kpo/zalecenia.txt 
+ */
+std::istream& operator >> (std::istream &Strm, Macierz &Mac);
+
+/*
+ * To przeciazenie trzeba opisac. Co ono robi. Jaki format
+ * danych akceptuje. Jakie jest znaczenie parametrow itd.
+ * Szczegoly dotyczace zalecen realizacji opisow mozna
+ * znalezc w pliku:
+ *    ~bk/edu/kpo/zalecenia.txt 
+ */
+std::ostream& operator << (std::ostream &Strm, const Macierz &Mac);
+
+
+#endif
---
+++ inc/Wektor.hh
@@ -0,0 +1,41 @@
+#ifndef WEKTOR_HH
+#define WEKTOR_HH
+
+#include "rozmiar.h"
+#include <iostream>
+
+
+/*
+ *  Tutaj trzeba opisac klase. Jakie pojecie modeluje ta klasa
+ *  i jakie ma glowne cechy.
+ */
+class Wektor {
+  /*
+   *  Tutaj trzeba wstawic definicje odpowiednich pol i metod prywatnych
+   */
+  public:
+  /*
+   *  Tutaj trzeba wstawic definicje odpowiednich metod publicznych
+   */    
+};
+
+
+/*
+ * To przeciazenie trzeba opisac. Co ono robi. Jaki format
+ * danych akceptuje. Jakie jest znaczenie parametrow itd.
+ * Szczegoly dotyczace zalecen realizacji opisow mozna
+ * znalezc w pliku:
+ *    ~bk/edu/kpo/zalecenia.txt 
+ */
+std::istream& operator >> (std::istream &Strm, Wektor &Wek);
+
+/*
+ * To przeciazenie trzeba opisac. Co ono robi. Jaki format
+ * danych akceptuje. Jakie jest znaczenie parametrow itd.
+ * Szczegoly dotyczace zalecen realizacji opisow mozna
+ * znalezc w pliku:
+ *    ~bk/edu/kpo/zalecenia.txt 
+ */
+std::ostream& operator << (std::ostream &Strm, const Wektor &Wek);
+
+#endif
